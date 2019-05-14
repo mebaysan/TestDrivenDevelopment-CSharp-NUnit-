c05a89a539426de4696f9922c2196a81c50fa64f
mebaysan <menesbaysan@gmail.com>
2019-05-14 16:26:47 +0300

    SetUp & TearDown

--- NUnitTestNotes/Methodlarim.Test/FirstMethodTests.cs
+++ NUnitTestNotes/Methodlarim.Test/FirstMethodTests.cs
@@ -13,10 +13,17 @@ namespace Methodlarim.Test
      * Act     -> eyleme geçtiğimiz kısımdır. Test etmek istediğimiz fonksiyonu(methodu) çağırdığımız ve sonucu aldığımız kısım.
      * Assert  -> testimizin başarılı olup olmadığını belirlediğimiz kısımdır.
      * Bu test projesinin referanslarına diğer Methodlarim adli projeyi dahil ettik.
+     * [SetUp] -> bu attribute her test methodundan önce çalışır.
+     * [TearDown]    -> bu attribute her test methodundan sonra çalışır.
      */
     [TestFixture] // bir class'ın test class olabilmesi için TestFixture attribute ile süslenmesi lazım. Diğer test frameworklerde bu attr isimleri dğeişebilir ama mantık aynı.
     public class FirstMethodTests
     {
+        [SetUp] // bu örnek içerisinde yaptığımız SetUp ve TearDown attribute'lar pek anlam ifade etmez fakat fikir olması açısından. Mesela 10 test methodu varsa ve hepsinde aynı fonksiyonu mutlaka bir kere çalıştırıyorsak bunları SetUp içine alabiliriz.
+        public void FirstLog()
+        {
+            Console.WriteLine("Test methodu çalışmaya başladı!");
+        }
         [Test] // bir methodun test methodu olabilmesi için Test attribute'dan beslenmesi(süslenmesi) lazım.
         public void ToplaMethoduCalisiyormu_Test() // Test methodunun isimlendirilmesi önemlidir. Gereksinim neyse onun adı_Test -> isimlendirme standardı
         {
@@ -33,6 +40,11 @@ namespace Methodlarim.Test
             // Assert
             Assert.AreEqual(beklenen, testSonuc); // İstediğimiz sonuç şu -> beklenen değer ile (ilk parametre) gerçekleşen değer (ikinci parametre) birbirine eşit olması.
 
+        }
+        [TearDown]
+        public void Logla()
+        {
+            Console.WriteLine("Test methodu çalışmayı bitirdi!");
         }
     }
 }
