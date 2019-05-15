374456b2cd84147ae720161d13c3d8293bd50473
mebaysan <menesbaysan@gmail.com>
2019-05-15 20:12:22 +0300

    msunit icin test ortamini olusturduk

---
+++ MSUnitTest/BussinessLayer/KullaniciManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace BussinessLayer
+{
+    public class KullaniciManager
+    {
+        public bool KullaniciEkle(string ad, string tel, string mail)
+        {
+            // iş kurallarımızı yazdık
+            if (ad.Length < 4) return false; // ad 4 karakterden küçükse kaydetme
+            if (!Regex.IsMatch(tel, "[0-9]")) return false;   // telefon istediğimiz pattern'e uygun olmalı
+            if (mail.Contains("@")) return false; // mail adresinde @ işareti geçmeli
+            return true;
+        }
+    }
+}
