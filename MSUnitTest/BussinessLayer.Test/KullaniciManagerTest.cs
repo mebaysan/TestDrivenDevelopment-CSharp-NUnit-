6f101a55c84f35022ca2aaa8af6cc85407f6e61b
mebaysan <menesbaysan@gmail.com>
2019-05-16 11:34:15 +0300

    other test attributes added

--- MSUnitTest/BussinessLayer.Test/KullaniciManagerTest.cs
+++ MSUnitTest/BussinessLayer.Test/KullaniciManagerTest.cs
@@ -15,6 +15,13 @@ namespace BussinessLayer.Test
             @"C:\Users\Yusuf\source\repos\TestDrivenDevelopment-CSharp.NUnit\MSUnitTest\BussinessLayer.Test\Kullanicilar.xml", "Kullanici", DataAccessMethod.Sequential)]
         // ilk parametre provider name, ikinci parametre data source(data'nın yeri)(ayrıca burada aynı proje altındaki kullanicilar.xml'i vermek için explorer'dan data üstüne tıkla properties->copy to output'u copy to always yap), ücüncü parametre tablo adı (xml ile çalıştığımızdan satır adı), dördüncü parametre dataları random mu yoksa sequential(sıralı) mı işlesin
         [TestMethod]
+        [Owner("Baysan")] // Owner attribute testi kimin yazdığını belirlememizi sağlar.
+        [TestCategory("Deneme")] // TestCategory methodu testlerimizi gruplamamızı sağlar.
+        [Priority(1)] // Priority attribute testlere öncelik sırası vermemizi sağlar.
+        [TestProperty("Güncelleyen","Enes")] // TestProperty attribute sınıflamamızı sağlar. İlk parametre isim, ikinci parametre bunu kimin yazdığıdır.
+        [Ignore] // Ignore attribute testleri atlamamızı(skip) sağlar.
+        [Timeout(1000)] // Timeout attribute parametre olarak milisaniye alır. Test eğer 1000 milisaniye üstünde olursa timeout(fail) olur.
+        [Description("bu bir data driven testtir!")] // Description attribute yorum eklememizi sağlar.
         public void KullaniciEkleTest()
         {
 
